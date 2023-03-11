a23d47fe98488dfeaf687315bdbc8d24df30cb47
Siracenco Serghei <siracencoserghei@gmail.com>
2023-03-11 15:37:16 +0100

    work for customizing the form button

--- src/index.js
+++ src/index.js
@@ -1,20 +1,40 @@
 import './sass/index.scss';
 import debounce from 'lodash.debounce';
-const axios = require('axios/dist/node/axios.cjs');
+const axios = require('axios');
 import Notiflix from 'notiflix';
 // Описан в документации
 import SimpleLightbox from "simplelightbox";
 // Дополнительный импорт стилей
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-import {btnSearch, galleryContainer, loadMoreBtn} from './partials/refs';
+import {btnSearchSubmit, formElem, galleryContainer, loadMoreBtn} from './partials/refs';
 
-btnSearch.addEventListener('submit', onSearchSubmit);
+// установка атрибута disabled на кнопке
+switchBtnSearchSubmit ();
+formElem.addEventListener('input', switchBtnSearchSubmit);
 
-function onSearchSubmit(evt) { evt => {
+function switchBtnSearchSubmit () {
+  const inputValue = formElem.elements['searchQuery'].value.trim();
+  if (inputValue) {
+    btnSearchSubmit.disabled = false;
+  } else {
+    btnSearchSubmit.disabled = true;
+  }
+};
+
+
+formElem.addEventListener('submit', onSearchSubmit);
+
+
+function onSearchSubmit(evt) {
     evt.preventDefault();
-    btnSearch.currentTarget.value
-}
-
+    const form = evt.currentTarget;
+    // const inputElement = form.querySelector('input[name="searchQuery"]');
+    const inputElement = form.elements['searchQuery'];
+    const inputValue = inputElement.value.trim();
+    if (!inputValue) {
+        return;
+    }
+    console.log(inputValue);
 };
 
--- src/partials/refs.js
+++ src/partials/refs.js
@@ -1,5 +1,7 @@
-const btnSearch = document.querySelector('#search-form');
+const formElem = document.querySelector('#search-form');
+const btnSearchSubmit = document.getElementById('search-form').querySelector('button[type="submit"]');
 const galleryContainer = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
 
-export {btnSearch, galleryContainer, loadMoreBtn}
+
+export {formElem,btnSearchSubmit, galleryContainer, loadMoreBtn}
